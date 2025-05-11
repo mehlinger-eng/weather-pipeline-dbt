13d28badfc15ab96a9ec49ac1605deac2643e900
mehlinger-eng <mehlinger.eng@gmail.com>
2025-05-11 12:38:11 -0600

    Add endpoint for sync

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from ingestion.ingest_api_to_firestore import main as ingest 
+from ingestion.ingest_api_to_firestore import main as ingest
+from sync.firestore_to_bigquery import main as sync
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/api/ingest")
 def run_ingestion():
     try:
         ingest()
@@ -12,3 +13,14 @@ def run_ingestion():
         import traceback
         traceback.print_exc()
         return {"status": "❌ Failed", "error": str(e)}, 500
+    
+@app.get("/api/sync")
+@app.post("/api/sync")
+def run_sync():
+    try:
+        sync()
+        return {"status": "✅ Sync complete"}
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        return {"status": "❌ Sync failed", "error": str(e)}, 500
